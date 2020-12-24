625709292b99c4c186bcc10b77310a64880874b8
Thorh13 <70460184+Thorh13@users.noreply.github.com>
2020-12-24 11:38:01 -0600

    Add files via upload
    Added more to "what to add" list

--- magic8.py
+++ magic8.py
@@ -1,19 +1,25 @@
 from random import randrange
 
 ##what to add
-#confirmation of if that is their question where if the answer no they will be asked again
-# after question has be answered if tey would like another question to be answered
-# reconize a 42 reference with custome reponse (done)
+# confirmation of if that is their question where if the answer no they will be asked again
+# after question has be answered ask if they would like another question to be answered(done)
+# recognize a 42 reference and respond with a custom reponse (done)
+# add custom list of "questions" to inform user to ask a question. (Requested by: (Requested by: Lamar "Goku" Brooks)
+# add custom list of "acknowledgements" when user wants to ask more questions. (Requested by: Lamar "Goku" Brooks)
 
-pattern_respond = 'Magic8: So that\'s what you want to know, is that right?\nMagic8: Well, {}'
+pattern_magic = '\n*POOF* MAGIC8 HAS JUST {} THE SHADOWS!!! *POOF*\n'
+pattern_respond = '\nMagic8: So that\'s what you want to know, is that right?\nMagic8: Well, {}\n'
 
 def magic8():
+    
     question = str(magic8_question())
     if "meaning of life" in question:
         response = pattern_respond.format(str(42))
     else:
         response = pattern_respond.format(str(magic8_response()))
     print(response)
+    magic8_again()
+    
 
 def magic8_question():
     print('Magic8: Ask me your wildest desires!')
@@ -33,4 +39,15 @@ def get_list_of_responses():
     infile.close()
     return list_responses
 
+def magic8_again():
+    ask_again = input('Would you like to ask Magic8 another question?(y/n) \t')
+    if ask_again == 'y':
+        print('\nMagic8: Oh, you want to know more do you!')
+        magic8()
+    else:
+        print(pattern_magic.format(str('DISAPPEARED INTO')))
+        return
+
+print(pattern_magic.format(str('APPEARED FROM')))
 magic8()
+print('This has been brought to you by: Thorh13')
